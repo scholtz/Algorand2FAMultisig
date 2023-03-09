013a52e0a1edc7709ddb4c563b5c74ed0c0dfb4f
Ludovit Scholtz <ludko2@gmail.com>
2023-03-09 20:58:11 +0100

    remove https redirect

--- Algorand2FAMultisig/Program.cs
+++ Algorand2FAMultisig/Program.cs
@@ -9,20 +9,14 @@ namespace Algorand2FAMultisig
             // Add services to the container.
 
             builder.Services.AddControllers();
-            // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
             builder.Services.AddEndpointsApiExplorer();
             builder.Services.AddSwaggerGen();
 
             var app = builder.Build();
 
             // Configure the HTTP request pipeline.
-            if (app.Environment.IsDevelopment())
-            {
-                app.UseSwagger();
-                app.UseSwaggerUI();
-            }
-
-            app.UseHttpsRedirection();
+            app.UseSwagger();
+            app.UseSwaggerUI();
 
             app.UseAuthorization();
 
