1ac43233f11314da62df64000fe7ef506759b0a8
CICD ZB <ludko2@gmail.com>
2023-05-22 07:24:11 +0200

    cicd deploy

--- k8s/update-config.sh
+++ k8s/update-config.sh
@@ -1,2 +1,5 @@
+kubectl apply -f deployment-testnet.yaml -n algo2fa
+kubectl rollout restart deployment/algo2fa-web-testnet-deployment -n algo2fa
+
 kubectl apply -f deployment-main.yaml -n algo2fa
 kubectl rollout restart deployment/algo2fa-web-main-deployment -n algo2fa
